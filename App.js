9b895348c381446ec8f70dd82deb92f5a291ca2a
Vitalii Havryliuk <vitalii.havryliuk@pragma-technologies.com>
2021-02-17 23:34:13 +0200

    Start token acount creation

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import './global';
 import React from 'react';
 import 'react-native-get-random-values';
 import {
@@ -11,19 +12,116 @@ import {
 
 import {
   Header,
-  LearnMoreLinks,
   Colors,
-  DebugInstructions,
-  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-import web3 from '@solana/web3.js';
+const solanaWeb3 = require('@solana/web3.js');
+import * as BufferLayout from 'buffer-layout';
+
+const TOKEN_PROGRAM_ID = new solanaWeb3.PublicKey('TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA');
+
+// const MEMO_PROGRAM_ID = new solanaWeb3.PublicKey('Memo1UhkJRfHyvLMcVucJwxXeuD728EqVDDwQDxFMNo');
+//
+// const MINT_LAYOUT = BufferLayout.struct([
+//   BufferLayout.blob(44),
+//   BufferLayout.u8('decimals'),
+//   BufferLayout.blob(37),
+// ]);
+
+const ACCOUNT_LAYOUT = BufferLayout.struct([
+  BufferLayout.blob(32, 'mint'),
+  BufferLayout.blob(32, 'owner'),
+  BufferLayout.nu64('amount'),
+  BufferLayout.blob(93),
+]);
+
+const LAYOUT = BufferLayout.union(BufferLayout.u8('instruction'));
+LAYOUT.addVariant(
+    0,
+    BufferLayout.struct([
+      BufferLayout.u8('decimals'),
+      BufferLayout.blob(32, 'mintAuthority'),
+      BufferLayout.u8('freezeAuthorityOption'),
+      BufferLayout.blob(32, 'freezeAuthority'),
+    ]),
+    'initializeMint',
+);
+LAYOUT.addVariant(1, BufferLayout.struct([]), 'initializeAccount');
+LAYOUT.addVariant(3, BufferLayout.struct([BufferLayout.nu64('amount')]), 'transfer');
+LAYOUT.addVariant(7, BufferLayout.struct([BufferLayout.nu64('amount')]), 'mintTo');
+LAYOUT.addVariant(8, BufferLayout.struct([BufferLayout.nu64('amount')]), 'burn');
+LAYOUT.addVariant(9, BufferLayout.struct([]), 'closeAccount');
+
+const instructionMaxSpan = Math.max(...Object.values(LAYOUT.registry).map((r) => r.span));
+
+function encodeTokenInstructionData(instruction) {
+  const b = Buffer.alloc(instructionMaxSpan);
+  const span = LAYOUT.encode(instruction, b);
+  return b.slice(0, span);
+}
+
+async function testWeb3() {
+  const secret = Buffer.from([184,234,94,108,74,201,179,86,173,136,230,45,12,108,66,181,77,14,211,111,58,168,52,107,214,173,83,53,61,158,61,118,55,203,30,99,89,70,138,32,202,42,222,88,93,51,242,193,94,51,43,225,255,106,36,30,93,224,10,118,117,123,43,221]);
+  const account = new solanaWeb3.Account(secret);
+  console.log('ACCOUNT: ', account.publicKey.toString());
+  const connection = new solanaWeb3.Connection('https://devnet.solana.com');
+
+  const accountData = await connection.getAccountInfo(account.publicKey);
+  console.log('ACCOUNT DATA: ', accountData);
+
+  const balance = await connection.getBalance(account.publicKey, 'max');
+  console.log('BALANCE: ', balance);
+
+  const lamportsForAccount = await connection.getMinimumBalanceForRentExemption(ACCOUNT_LAYOUT.span);
+  console.log('BALANCE FOR RENT: ', lamportsForAccount);
+
+  const transaction = new solanaWeb3.Transaction();
+
+  const newAccount = new solanaWeb3.Account();
+  console.log('NEW ACCOUNT PK: ', newAccount.publicKey);
+
+  transaction.add(
+      solanaWeb3.SystemProgram.createAccount({
+        fromPubkey: account.publicKey,
+        newAccountPubkey: newAccount.publicKey,
+        lamports: lamportsForAccount,
+        space: ACCOUNT_LAYOUT.span,
+        programId: new solanaWeb3.PublicKey(account.publicKey),
+      }),
+  );
+
+  const keys = [
+    { pubkey: newAccount.publicKey, isSigner: false, isWritable: true },
+    { pubkey: '3gJBsGKXS4cxW6DYqT3EnDfzshuWQ1QMrqMAUxgCsDKi', isSigner: false, isWritable: false },
+    { pubkey: account.publicKey, isSigner: false, isWritable: false },
+    { pubkey: solanaWeb3.SYSVAR_RENT_PUBKEY, isSigner: false, isWritable: false },
+  ];
+
+  const initializeAccountInstruction = new solanaWeb3.TransactionInstruction({
+    keys,
+    data: encodeTokenInstructionData({
+      initializeAccount: {},
+    }),
+    programId: new solanaWeb3.PublicKey(account.publicKey),
+  });
+
+  transaction.add(initializeAccountInstruction);
+
+  return await solanaWeb3.sendAndConfirmTransaction(
+      connection,
+      transaction,
+      [account, newAccount],
+      {
+        preflightCommitment: 'single',
+      },
+  );
+}
 
 const App: () => React$Node = () => {
-  const account = new web3.Account();
-  console.log('ACCOUNT: ', account.publicKey.toString());
-  const connection = new web3.Connection('https://devnet.solana.com');
-  console.log('CONNECTION: ', connection);
+  testWeb3()
+      .then(result => console.log(result))
+      .catch(error => console.log(error));
+
 
   return (
     <>
@@ -38,34 +136,6 @@ const App: () => React$Node = () => {
               <Text style={styles.footer}>Engine: Hermes</Text>
             </View>
           )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
         </ScrollView>
       </SafeAreaView>
     </>
--- metro.config.js
+++ metro.config.js
@@ -3,39 +3,21 @@ const nodeLibs = require('node-libs-react-native');
 nodeLibs.vm = require.resolve('vm-browserify');
 
 module.exports = (async () => {
-  const {
-    resolver: { sourceExts },
-  } = await getDefaultConfig();
-  return {
-    resolver: {
-      // Add cjs extension so stylis will load.
-      sourceExts: [...sourceExts, 'cjs'],
-      extraNodeModules: nodeLibs,
-    },
-    serializer: {
-      // From https://github.com/facebook/react-native/blob/v0.57.7/rn-get-polyfills.js
-      getPolyfills: () => [
-        /**
-         * We omit RN's Object.assign polyfill
-         * If we don't, then node_modules will be using RN's polyfill rather than ours.
-         */
-        // require.resolve('react-native/Libraries/polyfills/Object.es6.js'),
-        require.resolve('react-native/Libraries/polyfills/console.js'),
-        require.resolve('react-native/Libraries/polyfills/error-guard.js'),
-        require.resolve('react-native/Libraries/polyfills/Number.es6.js'),
-        require.resolve('react-native/Libraries/polyfills/String.prototype.es6.js'),
-        require.resolve('react-native/Libraries/polyfills/Array.prototype.es6.js'),
-        require.resolve('react-native/Libraries/polyfills/Array.es6.js'),
-        require.resolve('react-native/Libraries/polyfills/Object.es7.js'),
-      ]
-    },
-    transformer: {
-      getTransformOptions: async () => ({
-        transform: {
-          experimentalImportSupport: false,
-          inlineRequires: false,
+    const {
+        resolver: { sourceExts },
+    } = await getDefaultConfig();
+    return {
+        resolver: {
+            sourceExts: [...sourceExts, 'cjs'],
+            extraNodeModules: nodeLibs,
         },
-      }),
-    },
-  };
+        transformer: {
+            getTransformOptions: async () => ({
+                transform: {
+                    experimentalImportSupport: false,
+                    inlineRequires: false,
+                },
+            }),
+        },
+    };
 })();
